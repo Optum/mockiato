fbdc4929cb5f9e1bcae3444d17e945e7a6b39464
Pradeep Singh <pradeep_singh60@optum.com>
2018-11-20 05:51:14 +0530

    commiting directives js. Missed in last commit.

---
+++ public/js/app/directives.js
@@ -0,0 +1,30 @@
+var dir = angular.module("mockapp.directives",[])
+
+    .directive('integerNoDecimal', ['dirConstants', function(dirConstants) {
+        return {
+          require: 'ngModel',
+          link: function(scope, elm, attrs, ctrl) {
+            ctrl.$validators.integer = function(modelValue, viewValue) {
+              if (ctrl.$isEmpty(modelValue)) {
+                // consider empty models to be valid
+                return false;
+              }
+              var re = new RegExp("^[0-9]*$");
+              if (re.test(viewValue)) {
+                // it is valid
+                return true;
+              }
+      
+              // it is invalid
+              return false;
+            };
+          }
+        };
+      }]);
+
+
+//Put all the hard coding or constants here for directives.      
+dir.constant("dirConstants", {
+    //"INTEGER_REGEXP" : '/^-?\d+$/'
+    "INTEGER_REGEXP" : '/^[0-9]*$/'
+});
