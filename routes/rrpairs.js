ca8d7c28ea086d5cc0c2be498a4b2249986a473e
jweeks16 <joseph.weeks@optum.com>
2018-12-07 09:57:50 -0500

    fix a few more deepscan issues

--- routes/rrpairs.js
+++ routes/rrpairs.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
-
 const rrCtrl = require('../controllers/rrpairController');
 
 router.get('/:serviceId/rrpairs', rrCtrl.getPairsByServiceId);
