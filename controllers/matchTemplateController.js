c71518e4d5d0a317e31cfc029306e52ff0a98470
Legeyt <jlegeyt@ms.ds.uhc.com>
2019-02-12 15:41:32 -0600

    Added parsing for conditions

--- controllers/matchTemplateController.js
+++ controllers/matchTemplateController.js
@@ -1,4 +1,39 @@
+const xml2js = require('xml2js');
+const debug = require('debug')('matching');
+
+function mergeInOptions(oldOptions,newOptions){
+    for(let key1 in newOptions){
+        if(oldOptions[key1]){
+            if(Array.isArray(oldOptions[key1])){
+                oldOptions[key1] = oldOptions[key1].concat(newOptions[key1]);
+            }else if(typeof oldOptions == 'object'){
+                for(let key2 in newOptions[key1]){
+                    oldOptions[key1][key2] = newOptions[key1][key2];
+                }
+            }else{
+                oldOptions[key1] = newOptions[key1];
+            }
+        }else{
+            oldOptions[key1] = newOptions[key1];
+        }
+    }
+}
+
+
+function processCondition(field,conditionString,flatPayload){
+    var split = conditionString.split(":",2);
+    switch(split[0]){
+        case "map":
+            var map = {};
+            map[split[1]] = flatPayload[field];
+            return {map};
+        default:
+            return false;
+    }
+}
+
 function matchOnTemplate(template,rrpair,payload,reqData,path){
+    var returnOptions = {};
     if (rrpair.payloadType === 'XML') {
         xml2js.parseString(template, function(err, xmlTemplate) {
           if (err) {
@@ -14,6 +49,7 @@ function matchOnTemplate(template,rrpair,payload,reqData,path){
         }
         catch(e) {
           debug(e);
+          return false;
         }
       }
 
@@ -22,22 +58,37 @@ function matchOnTemplate(template,rrpair,payload,reqData,path){
       const flatReqData  = flattenObject(reqData);
 
       const trimmedPayload = {}; const trimmedReqData = {};
-        
+      var hasCondition = false;
       for (let field in flatTemplate) {
-        trimmedPayload[field] = flatPayload[field];
-        trimmedReqData[field] = flatReqData[field];
+
+        //If we have a condition here, handle its special properties
+        if(flatTemplate[field]){
+            var ret = processCondition(field,flatTemplate[field],flatPayload);
+            if(ret !== false){
+                mergeInOptions(returnOptions,ret);
+                hasCondition = true;
+            }else{
+                return false;
+            }
+        //Otherwise add this to the list to get literal equals'd
+        }else{
+            trimmedPayload[field] = flatPayload[field];
+            trimmedReqData[field] = flatReqData[field];
+        }
       }
       
       logEvent(path, rrpair.label, 'received payload (from template): ' + JSON.stringify(trimmedPayload, null, 2));
       logEvent(path, rrpair.label, 'expected payload (from template): ' + JSON.stringify(trimmedReqData, null, 2));
 
-      match = deepEquals(trimmedPayload, trimmedReqData);
+      if(!deepEquals(trimmedPayload, trimmedReqData)){
+          return false;
+      }
 
       // make sure we're not comparing {} == {}
-      if (match && JSON.stringify(trimmedPayload) === '{}') {
-        match = false;
+      if (!hasCondition && JSON.stringify(trimmedPayload) === '{}') {
+        return false;
       }
-      return match;
+      return returnOptions;
 }
 
 
