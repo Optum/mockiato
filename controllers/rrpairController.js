cc4cc489da0a4faa27ec3135cde462c4043d7423
Legeyt <jlegeyt@ms.ds.uhc.com>
2019-02-20 14:35:33 -0600

    Removed completely unused function

--- controllers/rrpairController.js
+++ controllers/rrpairController.js
@@ -27,41 +27,6 @@ function getPairsByServiceId(req, res) {
   });
 }
 
-function trimRequestData(template, rrpair) {
-  if (!template || !rrpair) {
-    return;
-  }
-  
-  xml2js.parseString(template, function(err, xmlTemplate) {
-    if (err) {
-      debug(err);
-      return;
-    }
-    template = xmlTemplate;
-  });
-
-  let reqData;
-  xml2js.parseString(rrpair.reqData, function(err, data) {
-    reqData = data;
-  });
-
-  // flatten request data
-  const flatTemplate = flattenObject(template);
-  const flatReqData  = flattenObject(reqData);
-  const trimmedReqData = {};
-    
-  // pull out the fields specified in the template
-  for (let field in flatTemplate) {
-    trimmedReqData[field] = flatReqData[field];
-  }
-
-  // unflatten the trimmed request data
-  const unflatReqData = unflattenObject(trimmedReqData);
-
-  return xmlBuilder.buildObject(unflatReqData);
-}
-
 module.exports = {
-  getPairsByServiceId: getPairsByServiceId,
-  trimRequestData: trimRequestData
+  getPairsByServiceId: getPairsByServiceId
 };
