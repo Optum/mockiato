2a3d5c62f98c01efdf2a1e70537fe7c361f5cd69
jweeks16 <joseph.weeks@optum.com>
2019-03-01 12:29:07 -0500

    add API route to expose MQ info, contd

--- controllers/mqInfoController.js
+++ controllers/mqInfoController.js
@@ -1,9 +1,34 @@
 const request = require('request');
 const MQInfo  = require('../models/mq/MQInfo');
+const debug = require('debug')('default');
 const mockiatoJmsUri = process.env.MOCKIATO_JMS_URI;
 
 function getMQInfo(req, res) {
-  res.json('STUB');
+  if (!mockiatoJmsUri) {
+    debug('MOCKIATO_JMS_URI not set');
+    handleError('Could not retrieve MQ info', res, 500);
+    return;
+  }
+
+  request(mockiatoJmsUri + '/env', function(err, resp, body) {
+    if (err) {
+      debug(err);
+      handleError('Could not retrieve MQ info', res, 500);
+      return;
+    }
+
+    let data = parseInfo(body);
+    
+    res.json(data);
+  });
+}
+
+function parseInfo(body) {
+  let data  = JSON.parse(body);
+  let info  = data['applicationConfig: [classpath:/application.yml]'];
+  let final = unflattenObject(info);
+
+  return final.mockiato.mq;
 }
 
 module.exports = {
