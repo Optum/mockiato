854a88256a4feb590fb8ab12b05953dfb4a7c3d5
Zak Parks <zachary.parks@optum.com>
2019-03-20 17:26:22 -0400

    cleaned up

--- public/js/app/controllers/rrPairs.js
+++ public/js/app/controllers/rrPairs.js
@@ -16,7 +16,6 @@ var ctrl = angular.module("mockapp.controllers")
                   }]
                 });
                 if (newItemNo > 10){
-                  console.log("hitting new if");
                   $scope.loadMore();
                   window.scrollTo(0, document.body.scrollHeight);
                 }
