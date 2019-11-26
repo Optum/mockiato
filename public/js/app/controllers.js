0e6f7d486808296bc202f17d692f6e7d8146a240
Pradeep Singh <pradeep_singh60@optum.com>
2019-11-27 01:52:34 +0530

    multi tabs implementation

--- public/js/app/controllers.js
+++ public/js/app/controllers.js
@@ -1294,16 +1294,34 @@ var ctrl = angular.module("mockapp.controllers", ['mockapp.services', 'mockapp.f
 
     }])
 
-  .controller("apiTestingController", ['$scope', '$http',
-      function ($scope,  $http) {    
-           $http.get('/api/report')
-          .then(function (response) {
-            $scope.reportingJson = response.data;
-          })
-          .catch(function (err) {
-            console.log(err);
-          });
-      }])
+  .controller('apiTestingController',['$scope',
+    function ($scope) {
+      /** holds tabs, we will perform repeat on this **/
+      $scope.tabs = [{
+        id: 1,
+        content: 'This is a default tab on load'
+      }]
+
+      $scope.counter = 1;
+      /** Function to add a new tab **/
+      $scope.addTab = function () {
+        $scope.counter++;
+        $scope.tabs.push({ id: $scope.counter, content: 'Any Content' });
+        $scope.selectedTab = $scope.tabs.length - 1; //set the newly added tab active. 
+      }
+
+      /** Function to delete a tab **/
+      $scope.deleteTab = function (index) {
+        $scope.tabs.splice(index, 1); //remove the object from the array based on index
+      }
+
+      $scope.selectedTab = 0; //set selected tab to the 1st by default.
+
+      /** Function to set selectedTab **/
+      $scope.selectTab = function (index) {
+        $scope.selectedTab = index;
+      }
+    }])
 
   .controller("updateController", ['$scope', '$q', '$http', '$routeParams', 'apiHistoryService', 'feedbackService', 'suggestionsService', 'helperFactory', 'commonCodeFactory', 'ctrlConstants', 'sutService', 'authService', "$location",'modalService', 'mqInfoFactory',
     function ($scope, $q, $http, $routeParams, apiHistoryService, feedbackService, suggestionsService, helperFactory, commonCodeFactory, ctrlConstants, sutService, authService, $location, modalService, mqInfoFactory) {
